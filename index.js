151aa4c6fa9d6ad314ee04b7aa45fcc6c2dc080c
Left4Dev <rafaelkudla@gmail.com>
2019-11-24 17:38:23 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,63 +3,75 @@ import './style.css';
 import * as PIXI from 'pixi.js'
 
 window.PIXI = PIXI;
-var app = new PIXI.Application();
-document.body.appendChild(app.view);
-
-app.stage.interactive = true;
-
-var container = new PIXI.Container();
-app.stage.addChild(container);
-
-var backgroundSprite = PIXI.Sprite.from('https://images.unsplash.com/photo-1543005472-1b1d37fa4eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80');
-container.addChild(backgroundSprite);
-backgroundSprite.x = 0;
-backgroundSprite.y = 0;
-
-var displacementSprite = PIXI.Sprite.from('https://images.unsplash.com/photo-1497044725446-4156b475ea88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80');
-displacementSprite.scale.y=10
-// Make sure the sprite is wrapping.
-displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-var displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-
-displacementSprite.position = backgroundSprite.position;
-
-app.stage.addChild(displacementSprite);
-
-backgroundSprite.filters = [displacementFilter];
-
-displacementFilter.scale.x = 50;
-displacementFilter.scale.y = 50;
-
-displacementSprite.y =  displacementSprite.height
 
 
 
-var displacementSprite1 = PIXI.Sprite.from('https://images.unsplash.com/photo-1497044725446-4156b475ea88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80');
-displacementSprite1.scale.y=5
-// Make sure the sprite is wrapping.
-displacementSprite1.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-var displacementFilter1 = new PIXI.filters.DisplacementFilter(displacementSprite1);
-
-displacementSprite1.position = backgroundSprite.position;
-
-app.stage.addChild(displacementSprite1);
-
-backgroundSprite.filters = [displacementFilter,displacementFilter1];
-
-displacementFilter1.scale.x = 50;
-displacementFilter1.scale.y = 50;
-
-// displacementSprite1.y =  displacementSprite1.height
 
 
-app.ticker.add(function() {
-    // Offset the sprite position to make vFilterCoord update to larger value. Repeat wrapping makes sure there's still pixels on the coordinates.
-    displacementSprite.y--;
-    // Reset x to 0 when it's over width to keep values from going to very huge numbers.
-    if (displacementSprite.y < 0) displacementSprite.y = 0;
+class Example {
+  constructor(){
 
-     displacementSprite1.x--;
-    // Reset x to 0 when it's over width to keep values from going to very huge numbers.
-    if (displacementSprite1.x < 0) displacementSprite1.x = 0;
-});
+    this.displacementSprites = []
+    this.displacementFilters = []
+    
+    this.app = new PIXI.Application({height:1200, width:700});
+    document.body.appendChild(this.app.view);
+
+    this.app.stage.interactive = true;
+
+    this.container = new PIXI.Container();
+    this.app.stage.addChild(this.container);
+    
+    this.create();
+  }
+  addSprite(){
+
+    this.backgroundSprite = PIXI.Sprite.from('https://images.unsplash.com/photo-1543005472-1b1d37fa4eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80');
+    this.container.addChild(this.backgroundSprite);
+    this.backgroundSprite.x = 0;
+    this.backgroundSprite.y = 0;
+  }
+  addDisplacementFilter(){
+    const displacementSprite = PIXI.Sprite.from('https://images.unsplash.com/photo-1497044725446-4156b475ea88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80');
+    displacementSprite.scale.y=20
+    
+    // Make sure the sprite is wrapping.
+    displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+    this.displacementSprites.push(displacementSprite)
+    this.displacementFilters.push(displacementFilter)
+
+    displacementSprite.position = this.backgroundSprite.position;
+
+    this.app.stage.addChild(displacementSprite);
+
+    displacementFilter.scale.x = 80;
+    displacementFilter.scale.y = 80;
+
+    displacementSprite.y =  displacementSprite.height
+
+  }
+  create(){
+    this.addSprite()
+    this.addDisplacementFilter()
+    this.addDisplacementFilter()
+    
+    this.backgroundSprite.filters = this.displacementFilters
+
+    this.app.ticker.add(()=> {
+        // Offset the sprite position to make vFilterCoord update to larger value. Repeat wrapping makes sure there's still pixels on the coordinates.
+        this.displacementSprites[0].y-=4;
+        // Reset x to 0 when it's over width to keep values from going to very huge numbers.
+        if (this.displacementSprites[0].y < 0) this.displacementSprites[0].y = 0;
+
+        this.displacementSprites[1].x-=4;
+        // Reset x to 0 when it's over width to keep values from going to very huge numbers.
+        if (this.displacementSprites[1].x < 0) this.displacementSprites[1].x = 0;
+    });
+    }
+
+
+}
+
+const ex = new Example()
